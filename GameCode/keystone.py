9af23ce9dddfc42deab561153fec46d8c419f567
Adam Dunlap <dunlapadam@gmail.com>
2015-04-11 02:38:51 -0700

    Added a method in keystone to set the Homography matrix

--- GameCode/keystone.py
+++ GameCode/keystone.py
@@ -5,8 +5,8 @@ class Keystone:
     def __init__(self, w, h, H=np.eye(3)):
         #self._far = 100
         #self._near = 120
-        #self._screenWidth = w
-        #self._screenHeight = h
+        self._screenWidth = w
+        self._screenHeight = h
         self._transformation = H
 
 
@@ -34,6 +34,14 @@ class Keystone:
     #    newbr = [xtransform(br[0], br[1]), ytransform(br[0], br[1])]
 
     #    return [newtl, newtr, newbl, newbr]
+
+    def setHomography(self, pt1, pt2):
+        srcPts = np.array([ (0,self._screenHeight), (self._screenWidth,self._screenHeight),
+                            (0,0), (self._screenWidth,0)], dtype=np.float32)
+        dstPts = np.array([ (0,self._screenHeight), (self._screenWidth,self._screenHeight),
+                            min(pt1,pt2), max(pt1,pt2)], dtype=np.float32)
+
+        self._transformation, _ = cv2.findHomography(srcPts, dstPts)
 
     def transformPoint(self, pt):
         t = self._transformation.dot(np.array(pt+[1]))
