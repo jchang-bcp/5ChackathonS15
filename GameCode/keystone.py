4a7736273ac9c58934500eed402e89dba2564bbc
Adam Dunlap <dunlapadam@gmail.com>
2015-04-11 02:30:37 -0700

    Adding keystone fixing features

--- GameCode/keystone.py
+++ GameCode/keystone.py
@@ -1,36 +1,46 @@
 import pygame
+import numpy as np
 
 class Keystone:
-    def __init__(self, w, h):
-        self._far = 100
-        self._near = 120
-        self._screenWidth = w
-        self._screenHeight = h
+    def __init__(self, w, h, H=np.eye(3)):
+        #self._far = 100
+        #self._near = 120
+        #self._screenWidth = w
+        #self._screenHeight = h
+        self._transformation = H
 
-    def xtransform(x, y):
-        mathx = x - self._screenWidth/2
-        mathy = self._screenHeight - y
-        return self._screenWidth/2 + (x - (x*(1-near/far)/self._screenHeight)*mathy)
 
-    def ytransform(x, y):
-        mathy = self._screenHeight - y
+    #def xtransform(self, x, y):
+    #    mathx = x - self._screenWidth/2
+    #    mathy = self._screenHeight - y
+    #    return self._screenWidth/2 + (x - (x*(1-near/far)/self._screenHeight)*mathy)
 
-    def makeShape(src):
-        """
-        Converts rectangle src to keystone-corrected polygon.
-        Polygon is returned as a list of vertices
-        """
-        tl = src.topleft
-        tr = src.topright
-        bl = src.bottomleft
-        br = src.bottomright
+    #def ytransform(self, x, y):
+    #    mathy = self._screenHeight - y
 
-        newtl = [xtransform(tl[0], tl[1]), ytransform(tl[0], tl[1])]
-        newtr = [xtransform(tr[0], tr[1]), ytransform(tr[0], tr[1])]
-        newbl = [xtransform(bl[0], bl[1]), ytransform(bl[0], bl[1])]
-        newbr = [xtransform(br[0], br[1]), ytransform(br[0], br[1])]
+    #def makeShape(self, src):
+    #    """
+    #    Converts rectangle src to keystone-corrected polygon.
+    #    Polygon is returned as a list of vertices
+    #    """
+    #    tl = src.topleft
+    #    tr = src.topright
+    #    bl = src.bottomleft
+    #    br = src.bottomright
 
-        return [newtl, newtr, newbl, newbr]
+    #    newtl = [xtransform(tl[0], tl[1]), ytransform(tl[0], tl[1])]
+    #    newtr = [xtransform(tr[0], tr[1]), ytransform(tr[0], tr[1])]
+    #    newbl = [xtransform(bl[0], bl[1]), ytransform(bl[0], bl[1])]
+    #    newbr = [xtransform(br[0], br[1]), ytransform(br[0], br[1])]
 
-    def polygon(surface, color, pointlist):
-        pygame.draw.polygon(surface, color, pointlist)
+    #    return [newtl, newtr, newbl, newbr]
+
+    def transformPoint(self, pt):
+        t = self._transformation.dot(np.array(pt+[1]))
+        print 't is', t
+        return [ t[0]/t[2], t[1]/t[2] ]
+
+    def polygon(self, surface, color, pointlist):
+        transformedPtList = [transformPoint(pt) for pt in pointlist]
+
+        pygame.draw.polygon(surface, color, transformedPtList)
